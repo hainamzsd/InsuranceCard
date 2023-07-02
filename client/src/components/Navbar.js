dca937b1606bc2fd7e304e3bc0b540b411494e09
D4ng <53222130+hainamzsd@users.noreply.github.com>
2023-07-02 21:04:48 +0700

    Added personal info page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,9 +3,14 @@ import '../css/central.css';
 import '../lib/animate/animate.min.css';
 import '../lib/owlcarousel/assets/owl.carousel.min.css';
 import '../lib/lightbox/css/lightbox.min.css';
+import PersonalInfo from '../pages/PersonalInfo';
+import App from '../App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 const Navbar = () => {
 
-    
+
+
   const [isTopOfPage, setIsTopOfPage] = useState(true);
 
   useEffect(() => {
@@ -24,32 +29,36 @@ const Navbar = () => {
     <nav className={`
             ${isTopOfPage ? '' : 'sticky-top'}
             navbar navbar-expand-lg navbar-light  px-4 px-lg-5 py-3 py-lg-0`}>
-                <a href="" className="navbar-brand p-0">
-                    <h1 className="m-0">DGital</h1>
-                    
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span className="fa fa-bars"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <div className="navbar-nav mx-auto py-0">
-                        <a href="index.html" className="nav-item nav-link active">Home</a>
-                        <a href="about.html" className="nav-item nav-link">About</a>
-                        <a href="service.html" className="nav-item nav-link">Service</a>
-                        <a href="project.html" className="nav-item nav-link">Project</a>
-                        <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                            <div className="dropdown-menu m-0">
-                                <a href="team.html" className="dropdown-item">Our Team</a>
-                                <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                                <a href="404.html" className="dropdown-item">404 Page</a>
-                            </div>
-                        </div>
-                        <a href="contact.html" className="nav-item nav-link">Contact</a>
-                    </div>
-                    <a href="" className="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
-                </div>
-            </nav>
+      <a href="" className="navbar-brand p-0">
+        <h1 className="m-0">Insurance Management  </h1>
+      </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span className="fa fa-bars"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav mx-auto py-0">
+          <Link to="/" className="nav-item nav-link">Home</Link>
+          <Link to="/PersonalInfo" className="nav-item nav-link">Personal Info</Link>
+      
+
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/PersonalInfo" element={<PersonalInfo />} />
+          </Routes>
+
+          <div className="nav-item dropdown">
+            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+            <div className="dropdown-menu m-0">
+              <a href="team.html" className="dropdown-item">Our Team</a>
+              <a href="testimonial.html" className="dropdown-item">Testimonial</a>
+              <a href="404.html" className="dropdown-item">404 Page</a>
+            </div>
+          </div>
+          <a href="contact.html" className="nav-item nav-link">Contact</a>
+        </div>
+        <a href="" className="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
+      </div>
+    </nav>
   )
 }
 
---
+++ client/src/pages/PersonalInfo.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import '../css/central.css';
+import '../lib/animate/animate.min.css';
+import '../lib/owlcarousel/assets/owl.carousel.min.css';
+import '../lib/lightbox/css/lightbox.min.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+const PersonalInfo = () => {
+    return (
+        <div class="container-xxl bg-white p-0">
+            <div classname="container-xxl position-relative p-0">
+                <Navbar></Navbar>
+
+                <div class="container-xxl py-5 bg-primary hero-header">
+                    <div class="container my-5 py-5 px-lg-5">
+                        <div class="row g-5 py-5">
+                            <div class="col-12 text-center">
+                                <h1 class="text-white animated slideInDown">Personal Info</h1>
+                                <hr class="bg-white mx-auto mt-0" style={{ width: "90px" }} />
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb justify-content-center">
+                                        <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                                        <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                                        <li class="breadcrumb-item text-white active" aria-current="page">Personal Info</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section>
+                <div class="container py-5">
+
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="card mb-4">
+                                <div class="card-body text-center">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+                                        class="rounded-circle img-fluid" style={{ width: "150px" }} />
+                                    <h5 class="my-3">John Smith</h5>
+                                    <p class="text-muted mb-1">Full Stack Developer</p>
+                                    <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                                    {/* <div class="d-flex justify-content-center mb-2">
+                                        <button type="button" class="btn btn-primary">Follow</button>
+                                        <button type="button" class="btn btn-outline-primary ms-1">Message</button>
+                                    </div> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="card mb-4">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Full Name</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">Johnatan Smith</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Email</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">example@example.com</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Phone</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">(097) 234-5678</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Mobile</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">(098) 765-4321</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <p class="mb-0">Address</p>
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default PersonalInfo
