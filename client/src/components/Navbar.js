0a178502c756735ba7e45b7354b72bda89fc2880
D4ng <53222130+hainamzsd@users.noreply.github.com>
2023-07-08 00:08:00 +0700

    small change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,9 +5,69 @@ import '../lib/owlcarousel/assets/owl.carousel.min.css';
 import '../lib/lightbox/css/lightbox.min.css';
 import PersonalInfo from '../pages/PersonalInfo';
 import ReactDOM from "react-dom/client";
+import { Button, Modal, Form,Nav, Tab } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 const Navbar = () => {
+  //login
+  const getUser = async () => {
+    try {
+      const response = await fetch('https://localhost:7184/api/Account/1');
+      if (!response.ok) {
+        throw new Error('API request failed');
+      }
+      const data = await response.json();
+      // Process the received data here
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
+  getUser();
+
+
+  const getHeaderText = () => {
+    return activeTab === 'login' ? 'Login' : 'Register';
+  };
+  const [showModal, setShowModal] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [activeTab, setActiveTab] = useState('login');
+  const handleTabChange = (tab) => setActiveTab(tab);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    console.log('Login clicked');
+  };
+
+  //register
+  const [usernameRegister, setUsernameRegister] = useState('');
+  const [emailRegister, setEmailRegister] = useState('');
+  const [passwordRegister, setPasswordRegister] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [citizenIdentification, setCitizenIdentification] = useState('');
+  const [rePasswordRegister, setRePasswordRegister] = useState('');
+
+  const handleEmailRegisterChange = (e) => setEmail(e.target.value);
+  const handlePasswordRegisterChange = (e) => setPassword(e.target.value);
+  const handleFullNameChange = (e) => setFullName(e.target.value);
+  const handleContactNumberChange = (e) => setContactNumber(e.target.value);
+  const handleAddressChange = (e) => setAddress(e.target.value);
+  const handleCitizenIdentificationChange = (e) => setCitizenIdentification(e.target.value);
+  const handleRePasswordRegisterChange = (e) => setRePasswordRegister(e.target.value);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Perform register logic here
+    console.log('Register clicked');
+  };
 
 
   const [isTopOfPage, setIsTopOfPage] = useState(true);
@@ -50,8 +110,134 @@ const Navbar = () => {
           </div>
           <Link to="/RequestContract" className="nav-item nav-link">Request Contract</Link>
         </div>
-        <a href="" className="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
+        <Button className="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block" variant="primary" onClick={handleShowModal}>
+        Login
+      </Button>
       </div>
+      
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{getHeaderText()}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Tab.Container activeKey={activeTab} onSelect={handleTabChange}>
+            <Nav variant="tabs" className="mb-3">
+              <Nav.Item>
+                <Nav.Link eventKey="login">Login</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="register">Register</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <Form onSubmit={handleLogin}>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </Form.Group>
+                  <Button className='mt-3' variant="primary" type="submit">
+                    Login
+                  </Button>
+                </Form>
+              </Tab.Pane>
+              <Tab.Pane eventKey="register">
+                <Form onSubmit={handleRegister}>
+                <Form.Group controlId="formRegisterEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={emailRegister}
+                      onChange={handleEmailRegisterChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      value={passwordRegister}
+                      onChange={handlePasswordRegisterChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterRePassword">
+                    <Form.Label>Re-enter Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Re-enter Password"
+                      value={rePasswordRegister}
+                      onChange={handleRePasswordRegisterChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterFullName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter full name"
+                      value={fullName}
+                      onChange={handleFullNameChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterContactNumber">
+                    <Form.Label>Contact Number</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter contact number"
+                      value={contactNumber}
+                      onChange={handleContactNumberChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterAddress">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter address"
+                      value={address}
+                      onChange={handleAddressChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formRegisterCitizenIdentification">
+                    <Form.Label>Citizen Identification</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter citizen identification"
+                      value={citizenIdentification}
+                      onChange={handleCitizenIdentificationChange}
+                    />
+                  </Form.Group>
+                  <Button className='mt-3' variant="primary" type="submit">
+                    Register
+                  </Button>
+                </Form>
+              </Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+        </Modal.Body>
+      </Modal>
     </nav>
   )
 }
